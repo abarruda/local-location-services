6f5d12b61d2facad4b52f28e43ebaded9fd13692
abarruda <aaron@abarruda.com>
2015-11-09 17:08:46 -0800

    Adding setup script to install dependencies.

---
+++ setup.sh
@@ -0,0 +1,5 @@
+sudo pip-3.2 install python-libnmap
+update-nmap-mac-prefixes.sh
+sudo cp /usr/share/nmap/nmap-mac-prefixes /usr/share/nmap/nmap-mac-prefixes.backup
+sudo mv nmap-mac-prefixes /usr/share/nmap/nmap-mac-prefixes
+
