060f119609c7b94dd3d9ae3706ee6808854657de
abarruda <aaron@abarruda.com>
2015-11-09 17:11:36 -0800

    Adding comments to setup script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
+# install python nmap library and also 
+# calls a script that updates nmap's mac address prefixes
+
 sudo pip-3.2 install python-libnmap
 update-nmap-mac-prefixes.sh
 sudo cp /usr/share/nmap/nmap-mac-prefixes /usr/share/nmap/nmap-mac-prefixes.backup
